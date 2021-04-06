36c02cb44f2f44f48dbfb7870a310183ea035f88
Tahir can can <>
2021-04-06 21:46:29 +0000

    mention euro muro vln

--- main.py
+++ main.py
@@ -5,14 +5,16 @@ import json
 
 client=discord.Client()
 
-def currency():
+def currency(unit):
   response=requests.get('http://data.fixer.io/api/latest?cbase=USD&access_key=35a3ad0f2f253d37131b68cd1b5953fc&&symbols=USD,TRY')
   json_data=json.loads(response.text)
-  usd=float(json_data['rates']['USD'])
   tr=float(json_data['rates']['TRY'])
+  if(unit=="euro"):
+    return tr
+  usd=float(json_data['rates']['USD'])
   currency=tr/usd
   print(currency)
-  currency=round(currency,4)
+  currency=round(currency,6)
   return currency
 
 @client.event
@@ -24,13 +26,11 @@ async def on_message(message):
   if message.author==client.user :
     return
   if message.content.startswith('!sa'):
-    await message.channel.send('Ve Aleyna Aleyküm Selam Mümin Kardeşim! ' + str(message.author.name)) 
+    await message.channel.send('Ve Aleyna Aleyküm Selam Mümin Kardeşim! ' + str(message.author.mention))
   if message.content.startswith('!dolar'):
-    await message.channel.send(currency())
-
-
-
-  
-
-
+    await message.channel.send(currency("dolar"))
+  if message.content.startswith('!euro'):
+    await message.channel.send(currency("euro"))
+  if message.content.startswith('!commands'):
+    await message.channel.send("Commands : \n > !sa\n > !dolar\n > !euro ")
 client.run(os.getenv('TOKEN'))
