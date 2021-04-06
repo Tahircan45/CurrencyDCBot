b0cafb8cf931c5cd9fce55cd88283cffbabcd368
Tahir can can <>
2021-04-06 23:24:58 +0000

    çalışır

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import discord
 import os
 import requests
 import json
+import random
+from keep_alive import keep_alive
 
 client=discord.Client()
 
@@ -15,6 +17,19 @@ def currency(unit):
   currency=tr/usd
   currency=round(currency,6)
   return "Yarım dolar :"+str(currency/2)
+
+def coinflip():
+  flip=random.randint(1,100)
+  if(flip==50):
+    return "Dik Geldi :D"
+  elif(flip>50):
+    return "tura"
+  else:
+    return "yazı"
+def genRand(mess):
+  x=mess.split(' ',1)
+  range=int(x[1])
+  return random.randint(0,range)
 
 @client.event
 async def on_ready():
@@ -31,5 +46,11 @@ async def on_message(message):
   if message.content.startswith('!euro'):
     await message.channel.send(currency("euro"))
   if message.content.startswith('!commands'):
-    await message.channel.send("Commands : \n > !sa\n > !dolar\n > !euro ")
+    await message.channel.send("Commands : \n > !sa\n > !dolar\n > !euro \n > !coinflip [ Yazı-%49,5 | Tura-%49,5 | Dik-%1 ] \n > !random sayıgir [örnek komut: '!random 100']")
+  if message.content.startswith('!coinflip'):
+    await message.channel.send(coinflip())
+  if message.content.startswith('!random'):
+    mess=str(message.content)
+    await message.channel.send(genRand(mess))
+keep_alive()
 client.run(os.getenv('TOKEN'))
